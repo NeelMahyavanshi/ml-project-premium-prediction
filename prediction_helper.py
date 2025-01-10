15d70952dec91b2fb2875ad79c8d9507dba7b595
Hinali Patel <henipatel768@gmail.com>
2025-01-09 21:43:08 -0500

    updated file path

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 
-model_young = joblib.load("app/artifacts/model_young.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
 model_rest = joblib.load("app/artifacts/model_rest.joblib")
 scaler_young = joblib.load("app/artifacts/scaler_young.joblib")
 scaler_rest = joblib.load("app/artifacts/scaler_rest.joblib")
